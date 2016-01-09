5346b19917b7dac924e5e46f884ac6fc6a59001f
jamesjbot <jongs.j@gmail.com>
2016-01-09 07:39:33 -0500

    Correct a comment and fix indentation and spring

--- MemeMe/DetailViewController.swift
+++ MemeMe/DetailViewController.swift
@@ -12,11 +12,10 @@ import UIKit
 
 class DetailViewController: UIViewController{
 
-
-
     @IBOutlet weak var detailImageView: UIImageView!
 
     var myMeme: Meme!
+    
     
     
     // Function needed to correctly fill this screen with image
@@ -28,22 +27,21 @@ class DetailViewController: UIViewController{
     }
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Setup the buttons for editExistingMeme and pullUpMemeEditor
-        
-            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain,
-            target: self,
-            action: "editExistingMeme")
+        // Setup the buttons for editExistingMeme
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain,
+            target: self,action: "editExistingMeme")
     }
+    
     
     
     func editExistingMeme(){
             let evc = storyboard!.instantiateViewControllerWithIdentifier("EditorViewController") as! EditorViewController
             evc.myMeme = myMeme!
             presentViewController(evc, animated: true, completion: nil)
-
     }
     
 
